ae4d2c638c84b236984ab09662dfe09f3cf1d30a
mjhemmerlein <megan.hemmerlein@colostate.edu>
2024-05-08 15:19:28 -0600

    adding log prefix

--- submit_snakemake.sh
+++ submit_snakemake.sh
@@ -4,6 +4,6 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=24 # modify this number to reflect how many cores you want to use (up to 24)
 #SBATCH --time=unlimited   # modify this to reflect how long to let the job go.
-#SBATCH --output=/home/mjhemm/projects/snakemake-single-cell/Results/logs/snakemake_%J.txt
+#SBATCH --output=/home/mjhemm/projects/snakemake-single-cell/Results/logs/log_snakemake_%J.txt
 
 snakemake --conda-frontend conda --cores 4 all
